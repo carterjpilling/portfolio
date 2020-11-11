b3c01e4fe68276d38c2b5113f5588266c47050ed
Cj <carterjpilling@gmail.com>
2020-11-11 10:01:48 -0700

    Added Redux

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,6 +92,10 @@ const logo_set = [
     name: 'Node.js'
   },
   {
+    src: require('../assets//images/portfolio/logos/Redux.png'),
+    name: 'Redux'
+  },
+  {
     src: require('../assets//images/portfolio/logos/PostgreSQL.png'),
     name: 'PostreSQL'
   },
