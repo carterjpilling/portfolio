08a36852eb686515601bffe9b32cdb317f889f9c
Cj <carterjpilling@gmail.com>
2021-01-17 09:31:25 -0700

    Addded GA

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Layout from '../components/Layout';
 import Scroll from '../components/Scroll';
@@ -10,9 +10,6 @@ import ContactUs from '../components/ContactUs';
 import Gallery from '../components/Gallery';
 // import CSS3 from '../assets//images/portfolio/logos/CSS3.png'
 // import Express from '../assets//images/portfolio/logos/Express.png'
-const img = [
-  { src: require('../assets/images/portfolio/fullsize/2.jpg') }
-]
 const img_set = [
   {
     src: require('../assets/img/portfolio.png'),
@@ -165,121 +162,126 @@ const logos = logo_set.map((e) => {
 })
 
 const IndexPage = () => (
-  <Layout>
-    <Header />
 
-    <header className="masthead">
-      <div className="container h-100">
-        <div className="row h-100 align-items-center justify-content-center text-center">
-          <div className="col-lg-10 align-self-end">
-            <h1 className="text-uppercase text-white font-weight-bold">
-              {config.heading}
-            </h1>
-            <hr className="divider my-4" />
-          </div>
-          <div className="col-lg-8 align-self-baseline">
-            <p className="text-white-75 font-weight-light mb-5">
-              {config.subHeading}
-            </p>
-            <Scroll type="id" element="about">
-              <a
-                className="btn btn-primary btn-xl js-scroll-trigger"
-                href="#about"
-              >
-                Find Out More
+  useEffect(() => {
+    ReactGA.initialize('UA-185129485-2')
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+  < Layout >
+  <Header />
+
+  <header className="masthead">
+    <div className="container h-100">
+      <div className="row h-100 align-items-center justify-content-center text-center">
+        <div className="col-lg-10 align-self-end">
+          <h1 className="text-uppercase text-white font-weight-bold">
+            {config.heading}
+          </h1>
+          <hr className="divider my-4" />
+        </div>
+        <div className="col-lg-8 align-self-baseline">
+          <p className="text-white-75 font-weight-light mb-5">
+            {config.subHeading}
+          </p>
+          <Scroll type="id" element="about">
+            <a
+              className="btn btn-primary btn-xl js-scroll-trigger"
+              href="#about"
+            >
+              Find Out More
               </a>
-            </Scroll>
-          </div>
+          </Scroll>
         </div>
       </div>
-    </header>
+    </div>
+  </header>
 
-    <section className="page-section bg-primary" id="about">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-lg-8 text-center">
-            <h2 className="text-white mt-0">About Me</h2>
-            <hr className="divider light my-4" />
-            <p className="text-white-50 mb-4">
-              After working in Sports Administration for several years, I knew I wanted to use my strengths to take on a new challenge. Devmountain's coding bootcamp allowed me to pivot my future and career for the better. I have loved every minute of problem solving, finding solutions, learning new technologies, and improving app efficiencies. As a developer, I want to use technology to make the world a better place for current and future generations.<br /><br /> When I'm not coding, I'm spending time with my dog in the great outdoors, playing board games, and cheering on my favorite sport teams. If you have any questions, please don't hesitate to reach out!
+  <section className="page-section bg-primary" id="about">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-8 text-center">
+          <h2 className="text-white mt-0">About Me</h2>
+          <hr className="divider light my-4" />
+          <p className="text-white-50 mb-4">
+            After working in Sports Administration for several years, I knew I wanted to use my strengths to take on a new challenge. Devmountain's coding bootcamp allowed me to pivot my future and career for the better. I have loved every minute of problem solving, finding solutions, learning new technologies, and improving app efficiencies. As a developer, I want to use technology to make the world a better place for current and future generations.<br /><br /> When I'm not coding, I'm spending time with my dog in the great outdoors, playing board games, and cheering on my favorite sport teams. If you have any questions, please don't hesitate to reach out!
 
 
             </p>
-            <Scroll type="id" element="portfolio">
-              <a
-                className="btn btn-light btn-xl js-scroll-trigger"
-                href="#portfolio"
-              >
-                See my projects below!
+          <Scroll type="id" element="portfolio">
+            <a
+              className="btn btn-light btn-xl js-scroll-trigger"
+              href="#portfolio"
+            >
+              See my projects below!
               </a>
-            </Scroll>
+          </Scroll>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section className="page-section" id="services">
+    <div className="container">
+      <h2 className="text-center mt-0">Philosophy</h2>
+      <hr className="divider my-4" />
+      <div className="row">
+        <div className="col-lg-3 col-md-6 text-center">
+          <div className="mt-5">
+            <i className="fas fa-4x fa-gem text-primary mb-4"></i>
+            <h3 className="h4 mb-2">Hard Worker</h3>
+            <p className="text-muted mb-0">
+              Problem solving is my motivater.
+              </p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6 text-center">
+          <div className="mt-5">
+            <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+            <h3 className="h4 mb-2">Qualified</h3>
+            <p className="text-muted mb-0">
+              Check out my skills below, I know my stuff!
+              </p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6 text-center">
+          <div className="mt-5">
+            <i className="fas fa-4x fa-globe text-primary mb-4"></i>
+            <h3 className="h4 mb-2">Learner</h3>
+            <p className="text-muted mb-0">
+              I love researching, code-referencing, and learning. Google is my BFF and I'm not afraid to use it.
+              </p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6 text-center">
+          <div className="mt-5">
+            <i className="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 className="h4 mb-2">Relatable</h3>
+            <p className="text-muted mb-0">
+              I got loads of colleagues and friends. Just check out my MySpace.
+              </p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section className="page-section" id="services">
-      <div className="container">
-        <h2 className="text-center mt-0">Philosophy</h2>
-        <hr className="divider my-4" />
-        <div className="row">
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-gem text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Hard Worker</h3>
-              <p className="text-muted mb-0">
-                Problem solving is my motivater.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Qualified</h3>
-              <p className="text-muted mb-0">
-                Check out my skills below, I know my stuff!
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-globe text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Learner</h3>
-              <p className="text-muted mb-0">
-                I love researching, code-referencing, and learning. Google is my BFF and I'm not afraid to use it.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-heart text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Relatable</h3>
-              <p className="text-muted mb-0">
-                I got loads of colleagues and friends. Just check out my MySpace.
-              </p>
-            </div>
-          </div>
-        </div>
+  <section id="portfolio">
+    <Gallery images={img_set} />
+  </section>
+
+  <section className="page-section bg-dark text-white">
+    <div className="container text-center">
+      <h2 className="mb-4">Skills</h2>
+      <div className='skills-logo-container'>
+        {logos}
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section id="portfolio">
-      <Gallery images={img_set} />
-    </section>
+  <ContactUs />
 
-    <section className="page-section bg-dark text-white">
-      <div className="container text-center">
-        <h2 className="mb-4">Skills</h2>
-        <div className='skills-logo-container'>
-          {logos}
-        </div>
-      </div>
-    </section>
-
-    <ContactUs />
-
-    {/* <Footer /> */}
-  </Layout>
+    {/* <Footer /> */ }
+  </ >
 );
 
 export default IndexPage;
